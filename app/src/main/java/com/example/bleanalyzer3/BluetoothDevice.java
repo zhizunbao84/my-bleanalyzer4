dffcccba49fe9ec03b2ed27cb08eca8fb24c7745
zhizunbao84 <zhizunbao84@hotmail.com>
2025-11-19 14:11:42 +0800

    Create BluetoothDevice.java

---
+++ app/src/main/java/com/example/bleanalyzer3/BluetoothDevice.java
@@ -0,0 +1,11 @@
+package com.example.bleanalyzer3;
+
+public class BluetoothDevice {
+    public final String mac;
+    public final String alias;
+
+    public BluetoothDevice(String mac, String alias) {
+        this.mac = mac;
+        this.alias = alias;
+    }
+}
