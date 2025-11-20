6d87dca7829f88e812afa20b2e3b8033fda8b1e7
zhizunbao84 <zhizunbao84@hotmail.com>
2025-11-20 11:06:18 +0800

    Update MqttPublisher.java

--- app/src/main/java/com/example/bleanalyzer3/MqttPublisher.java
+++ app/src/main/java/com/example/bleanalyzer3/MqttPublisher.java
@@ -6,19 +6,32 @@ import org.eclipse.paho.client.mqttv3.*;
 public class MqttPublisher {
     private static final String TAG = "MqttPublisher";
 
+    /**
+     * 发布一次温湿度/电池数据
+     * @param cfg        配置对象（外部 ini）
+     * @param alias      设备别名（用于 topic）
+     * @param temp       温度
+     * @param humi       湿度
+     * @param batt       电池 %
+     */
     public static void publish(ConfigIni cfg, String alias,
                                float temp, float humi, int batt) {
-        String broker = cfg.mqttBroker;
-        String clientId = cfg.mqttClientId + "_" + System.currentTimeMillis();
-        String topicTemp = cfg.mqttTopicPrefix + "/" + alias + "/temperature";
-        String topicHumi = cfg.mqttTopicPrefix + "/" + alias + "/humidity";
-        String topicBatt = cfg.mqttTopicPrefix + "/" + alias + "/battery";
+
+        String broker      = cfg.getMqttBroker();
+        String clientId    = cfg.getMqttClientId() + "_" + System.currentTimeMillis();
+        String user        = cfg.getMqttUser();
+        String pass        = cfg.getMqttPass();
+        String prefix      = cfg.getMqttTopicPrefix();
+
+        String topicTemp = prefix + "/" + alias + "/temperature";
+        String topicHumi = prefix + "/" + alias + "/humidity";
+        String topicBatt = prefix + "/" + alias + "/battery";
 
         try {
-            MqttClient client = new MqttClient(broker, clientId, new org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence());
+            MqttClient client = new MqttClient(broker, clientId, new MqttDefaultFilePersistence());
             MqttConnectOptions options = new MqttConnectOptions();
-            options.setUserName(cfg.mqttUser);
-            options.setPassword(cfg.mqttPass.toCharArray());
+            options.setUserName(user);
+            options.setPassword(pass.toCharArray());
             options.setCleanSession(true);
             client.connect(options);
 
@@ -28,7 +41,7 @@ public class MqttPublisher {
 
             client.disconnect();
         } catch (MqttException e) {
-            Log.e(TAG, "MQTT error", e);
+            Log.e(TAG, "MQTT publish error", e);
         }
     }
 }
