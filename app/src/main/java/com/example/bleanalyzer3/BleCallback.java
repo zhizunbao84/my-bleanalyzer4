287ef1f80a5bbb88ca8f3db4cca7c904258b7f85
zhizunbao84 <zhizunbao84@hotmail.com>
2025-11-20 14:57:36 +0800

    Create BleCallback.java

---
+++ app/src/main/java/com/example/bleanalyzer3/BleCallback.java
@@ -0,0 +1,9 @@
+package com.example.bleanalyzer3;
+
+public interface BleCallback {
+    /* 温度/湿度/电池数据 */
+    void onData(String mac, String alias, float temp, float humi, int batt);
+
+    /* 原始报文（可选） */
+    void onRaw(String mac, String hex);
+}
