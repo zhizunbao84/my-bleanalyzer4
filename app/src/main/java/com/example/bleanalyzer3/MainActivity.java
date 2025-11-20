2a74dd2cf33af40e31640b78e112a78042c72742
zhizunbao84 <zhizunbao84@hotmail.com>
2025-11-20 16:29:32 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/bleanalyzer3/MainActivity.java
+++ app/src/main/java/com/example/bleanalyzer3/MainActivity.java
@@ -28,6 +28,20 @@ public class MainActivity extends AppCompatActivity implements BleCallback {
     private StringBuilder sb = new StringBuilder();
     private BleScanner scanner;
 
+    private BleCallback bleCallback = new BleCallback() {
+        @Override
+        public void onData(String mac, String alias, float temp, float humi, int batt) {
+            // 温度/湿度回调
+            String log = "★ mac="+ mac + " alias=" + alias + "  温度=" + temp + "℃  湿度=" + humi + "%  电池=" + batt + "%";
+            log(log);        
+        }
+    
+        @Override
+        public void onRaw(String hex) {
+            // 原始报文回调
+            log(hex);
+        }
+    };
     /* ===================== 日志 ===================== */
     public void log(final String txt) {
         runOnUiThread(() -> {
@@ -132,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements BleCallback {
         ConfigIni cfg = ConfigIni.getInstance(this);
         List<BluetoothDevice> devices = cfg.getBluetoothDevices();
         int intervalSec = cfg.getScanIntervalSec();
-        scanner = new BleScanner(this, devices, this, intervalSec);
+        scanner = new BleScanner(this, devices, bleCallback, intervalSec);
         
         if (scanner == null) {
             toast("BluetoothLeScanner 为空");
@@ -198,17 +212,4 @@ public class MainActivity extends AppCompatActivity implements BleCallback {
             scanner.stop();
     }
 
-    @Override
-    public void onData(String mac, String alias, float temp, float humi, int batt) {
-        // 温度/湿度回调
-        // 打印 + MQTT
-        String log = "★ mac="+ mac + " alias=" + alias + "  温度=" + temp + "℃  湿度=" + humi + "%  电池=" + batt + "%";
-        log(log);        
-    }
-
-    @Override
-    public void onRaw(String hex) {
-        // 原始报文回调
-        log(hex);
-    }
 }
